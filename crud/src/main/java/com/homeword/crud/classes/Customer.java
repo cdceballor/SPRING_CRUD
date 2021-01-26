f504e12d87cc4b97365a0bb1dbc39af8d0e55c17
cdceballor <cdceballor@eafit.edu.co>
2021-01-26 13:42:52 -0500

    withou delete branche

---
+++ crud/src/main/java/com/homeword/crud/classes/Customer.java
@@ -0,0 +1,13 @@
+package com.homeword.crud.classes;
+
+public class Customer {
+	public static void main (String args[]) {
+		suma();
+	}
+	public static int suma() {
+		int a=3;
+		int b=3;
+		int suma = a +b;
+		return suma;
+	}
+}
