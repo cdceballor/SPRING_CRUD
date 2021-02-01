ae141a451acd2dd33a6867a2f054749ed4aebe1e
cdceballor <cdceballor@eafit.edu.co>
2021-01-31 20:39:06 -0500

    No working the search bar yet

---
+++ project/src/main/java/com/project/project/interService/interfaceService.java
@@ -0,0 +1,13 @@
+package com.project.project.interService;
+import java.util.List;
+import java.util.Optional;
+import com.project.project.model.Person;
+
+public interface interfaceService {
+	public List<Person> list();
+	public Optional<Person>listPersonById(int id);
+	public int save(Person person);
+	public void delete(int id);
+	public List<Person> findByName(String name);
+}
+
